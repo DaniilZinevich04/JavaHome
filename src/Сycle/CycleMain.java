dd98959f38c9340dbbbcbcbada575278d096ebfb
Daniil <danchik20141@outlook.com>
2020-02-22 23:08:47 +0200

    2

---
+++ src/Сycle/CycleMain.java
@@ -0,0 +1,44 @@
+package Сycle;
+
+import java.util.Scanner;
+
+public class CycleMain {
+    public static void main(String[] args) {
+        Scanner r = new Scanner(System.in);
+        {
+        /*
+        System.out.println(" Таблица умножения ");
+        System.out.print("по вертикали - ");
+        int n = r.nextInt();
+        System.out.print("по горизонтали - ");
+        int l = r.nextInt();
+        for (int i = 1; i < n + 1; i++) {
+            for (int k = 1; k < l + 1; k++) {
+                System.out.printf("%5d", (k * i));
+            }
+            System.out.println("");
+        }
+
+        int a = r.nextInt();//Переможение 1*2*3
+        int f = 1;
+        for (int i = 1; a > 0; a--) {
+            f = f * a;
+        }
+        System.out.println(f);
+
+
+
+        String a = r.next();// попытка на перемножения 9210 - 9*2*1*0
+        String b = r.next();
+        int [] mass = new int [0];
+        System.out.println(mass.length);
+        int [] fhg = new int [] {Integer.parseInt(a), Integer.parseInt(b),7};
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            System.out.println(fhg[i]);
+        }
+
+         */
+        }
+    }
+}
