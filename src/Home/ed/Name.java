32e62c43a65cd3bf9c528606867cf69c1faa8184
Kost911 <Powerofthekee@mail.ru>
2020-02-22 16:51:07 +0300

    ChangeNameMain

--- src/Home/ed/Name.java
+++ src/Home/ed/Name.java
@@ -2,20 +2,22 @@ package Home.ed;
 import java.util.Scanner;
 public class Name {
     public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
         System.out.print("Добро пожаловать \n Как вас зовут? ");
-        String name = a.next();
-        String A ="Вася";
-        String B =  "Анатасия ";
-        System.out.println(A);
-        if (name == A){
-            System.out.println("Как дела, дружище ?");}
-        else if (name == B) {
+       Scanner Consol = new Scanner(System.in);
+        String name = Consol.next();
+        String A = "Вася";
+        String C= "";
+        String B = "Настя";
+        if (name.equalsIgnoreCase(A)) {
+            System.out.println("Как дела, дружище ?");
+        } else if (name.equalsIgnoreCase(B)) {
             System.out.println("Что нового красавица ?");
+        } else
+        {
+            System.out.println(name + ", кто ты есть по жизни ?");
+
+
         }
-
-
-
-
     }
 }
+
